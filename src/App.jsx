ddc563cbf33fe0a43158f71db6d68de98e8cca07
thismax <max.g.jacobs@gmail.com>
2017-11-30 21:17:30 -0800

    setting up basic render

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    HI IT ME
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
